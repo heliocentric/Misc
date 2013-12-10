492951034c46697ea4974ad06569a2c77a4c4db7
Dylan Cochran <heliocentric@gmail.com>
2013-12-09 21:26:34 -0500

    Add flags to all gcc instances

--- multibootexample/build.sh
+++ multibootexample/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 nasm -f elf start.asm
-gcc -m32 -c kernel.c
+gcc -m32 -ffreestanding -fno-builtin -nostdlib -nostartfiles -c kernel.c
 gcc -m32 -Xlinker -T -Xlinker ldscript -ffreestanding -fno-builtin -nostdlib -nostartfiles -s start.o kernel.o -o miniKernel
